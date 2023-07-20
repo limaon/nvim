aa921d25204dbc4f5e35c183894c6b5aea2aedf0
Myles Mo <mylesmo.ash@gmail.com>
2023-07-20 10:30:35 +0800

    fix(matchup): set matchparen_deferred to true

--- lua/custom/plugins/editing.lua
+++ lua/custom/plugins/editing.lua
@@ -76,6 +76,7 @@ local M = {
       { "<leader>;", "<plug>(matchup-%)", desc = "Find a match" },
     },
     config = function()
+      vim.g.matchup_matchparen_deferred = 1
       vim.g.matchup_matchparen_offscreen = {
         method = "popup",
         fullwidth = true,
