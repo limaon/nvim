daa941cd48e5f8676f620937b847d388dfca2571
Alvaro Oliveira <51033564+limaon@users.noreply.github.com>
2023-08-17 19:09:32 -0400

    refactor(git-plugin): reorganize keymaps and options

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,34 +1,44 @@
 local M = {
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    signs = {
-      add = { text = "▏" },
-      change = { text = "▏" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "▏" },
-      untracked = { text = "▏" },
+  {
+    "tpope/vim-fugitive",
+    keys = {
+      { "<leader>gs", "<Cmd>Git<CR>", desc = "Git status" },
     },
-    current_line_blame = true,
-    current_line_blame_opts = { virt_text_priority = 100 },
-    current_line_blame_formatter = " <author>, <author_time> · <summary> ",
-    preview_config = { border = moduleObject.styles.border },
-    on_attach = function(bufnr)
-      local gs = package.loaded.gitsigns
+  },
 
-      local function keymap(mode, lhs, rhs, desc)
-        vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-      end
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        -- add = { text = "▏" },
+        -- change = { text = "▏" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        -- changedelete = { text = "▏" },
+        -- untracked = { text = "▏" },
+      },
+      current_line_blame = false,
+      current_line_blame_opts = { virt_text_priority = 100 },
+      current_line_blame_formatter = " <author>, <author_time> · <summary> ",
+      preview_config = { border = moduleObject.styles.border },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
 
-      -- Navigation
-      keymap("n", "[g", gs.prev_hunk, "Prev git hunk")
-      keymap("n", "]g", gs.next_hunk, "Next git hunk")
-      -- Actions
-      keymap("n", "<leader>gp", gs.preview_hunk, "Preview hunk")
-      -- Text object
-      keymap({ "o", "x" }, "ig", ":<C-U>Gitsigns select_hunk<CR>", "Select git hunk")
-    end,
+        local function keymap(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+        end
+
+        -- Navigation
+        keymap("n", "[g", gs.prev_hunk, "Prev git hunk")
+        keymap("n", "]g", gs.next_hunk, "Next git hunk")
+        -- Actions
+        keymap("n", "<leader>gp", gs.preview_hunk, "Preview hunk")
+        -- Text object
+        keymap({ "o", "x" }, "ig", ":<C-U>Gitsigns select_hunk<CR>", "Select git hunk")
+      end,
+    },
+
   },
 }
 
